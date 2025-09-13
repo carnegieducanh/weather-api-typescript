6f6228637b4591c7d58e492d388591d92f214cf2
Huy Duc Anh <huyducanh@gmail.com>
2025-09-13 20:24:24 +0900

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,28 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+    <main className="flex h-[100vh] w-full items-center justify-center bg-gradient-to-br from-sky-400 via-rose-400 to-lime-400">
+      <section className="backdrop-blur-ls flex h-full w-full flex-col items-center justify-center rounded bg-white bg-opacity-20 p-4 text-center text-zinc-700 drop-shadow-lg md:max-w-[500px] md:px-10 lg:h-[500px] lg:p-24">
+        <h1 className="text-4xl font-thin">
+          Weather <span className="font-black">Forecast</span>
+        </h1>
+
+        <p className="mt-2 text-sm">
+          Enter below a place you want to know the weather of and select an
+          option from the dropdown
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+        <input
+          type="text"
+          value={''}
+          className="rounded-l-md border-2 border-white px-2 py-1"
+        />
+
+        <button className="cursor-pointer rounded-r-md border-2 border-zinc-100 px-2 py-1 text-zinc-100 hover:border-zinc-500 hover:text-zinc-500">
+          search
+        </button>
+      </section>
+    </main>
+  )
 }
 
-export default App;
+export default App
